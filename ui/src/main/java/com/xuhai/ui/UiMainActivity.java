f9e17381ed52b366d06cb1b3ae75a87e8058c01e
Xu Hai <hxu15@best-inc.com>
2020-04-26 11:55:33 +0800

    Initial commit

---
+++ ui/src/main/java/com/xuhai/ui/UiMainActivity.java
@@ -0,0 +1,13 @@
+package com.xuhai.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UiMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_main);
+    }
+}
