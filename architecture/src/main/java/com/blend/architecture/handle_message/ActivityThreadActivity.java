b9e774ff19a0953039e410fdba16c353189ac268
BlendAndroid <1059152860@qq.com>
2020-06-03 19:49:18 +0800

    阻塞，挂起，休眠的解释

--- architecture/src/main/java/com/blend/architecture/handle_message/ActivityThreadActivity.java
+++ architecture/src/main/java/com/blend/architecture/handle_message/ActivityThreadActivity.java
@@ -1,14 +1,18 @@
 package com.blend.architecture.handle_message;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.blend.architecture.R;
 
 /**
- * TODO:
+ * TODO:Loop会造成CPU挂起，是一种主动的状态。
  * 问题：阻塞，挂起，休眠的区别？
+ * 睡眠和挂起是两种行为，阻塞则是一种状态。
+ * 挂起：会主动释放CPU，也会主动占用CPU
+ * sleep:不释放锁，但是释放CPU，时间到了执行CPU任务
+ * 阻塞：被动的释放CPU，但是一旦情况满足，又会继续执行CPU任务
  */
 public class ActivityThreadActivity extends AppCompatActivity {
 
