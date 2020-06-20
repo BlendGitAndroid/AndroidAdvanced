b7b1f0a135841c6ce076b87e8071f6e140e97b33
BlendAndroid <1059152860@qq.com>
2020-06-20 19:27:57 +0800

    修改Service包

---
+++ userservice/src/main/java/com/example/userservice/UserServiceActivity.java
@@ -0,0 +1,13 @@
+package com.example.userservice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UserServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_service);
+    }
+}
