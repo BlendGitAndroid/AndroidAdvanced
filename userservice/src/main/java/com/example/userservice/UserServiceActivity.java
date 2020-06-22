b9957dfef1af41c4777f962a5b704f5b2800c9aa
BlendAndroid <1059152860@qq.com>
2020-06-22 23:45:38 +0800

    装备讲解设计模式

--- userservice/src/main/java/com/example/userservice/UserServiceActivity.java
+++ userservice/src/main/java/com/example/userservice/UserServiceActivity.java
@@ -1,13 +1,71 @@
 package com.example.userservice;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class UserServiceActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class UserServiceActivity extends Activity {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_service);
+        //策略模式
+        mButton = findViewById(R.id.btn);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void test(){
+
+        //单例模式
+        WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+
+        //Builder模式
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("test").setMessage("message").show();
+
+        //原型模式
+        Intent intent = new Intent();
+        intent.clone();
+
+        //工厂方法模式
+        setContentView(new TextView(this));
+
+
+        //迭代器模式
+        ArrayList<String> arrayList = new ArrayList<>();
+        Iterator iterator = arrayList.iterator();
+
+        HashMap<String,Integer> hashMap = new HashMap<>();
+        hashMap.entrySet().iterator();
+    }
+
+    //备忘录模式
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
