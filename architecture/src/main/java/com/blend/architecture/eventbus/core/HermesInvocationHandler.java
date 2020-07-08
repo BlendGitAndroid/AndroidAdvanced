3cde432ceedcf13c3bfe2d8658bf9e985e3ed03c
BlendAndroid <1059152860@qq.com>
2020-07-08 19:22:19 +0800

    完善Hermes EventBus，但是不明白动态代理的原理

--- architecture/src/main/java/com/blend/architecture/eventbus/core/HermesInvocationHandler.java
+++ architecture/src/main/java/com/blend/architecture/eventbus/core/HermesInvocationHandler.java
@@ -1,7 +1,6 @@
 package com.blend.architecture.eventbus.core;
 
 import android.text.TextUtils;
-import android.util.Log;
 
 import com.blend.architecture.eventbus.Responce;
 import com.blend.architecture.eventbus.response.ResponceBean;
@@ -30,22 +29,23 @@ public class HermesInvocationHandler implements InvocationHandler {
     创建一个类，实现这个方法，利用反射在invoke()方法里实现需求
 
     三个参数：
-    Object proxy：就是代理对象，但可以使用getClass()方法，得到proxy的Class类从而取得实例的类信息，如方法列表，annotation等
-    Method method：被动态代理类调用的方法，从中可得到方法名，参数类型，返回类型等等
-    Object[] args：方法中的参数
+    Object proxy：指代我们所代理的那个真实对象
+    Method method：指代的是我们所要调用真实对象的某个方法的Method对象
+    Object[] args：指代的是调用真实对象某个方法时接受的参数
      */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        Log.i(TAG, "invoke:-------> " + method.getName());
         //生成单例，返回客户端
         Responce responce = Hermes.getDefault().sendObjectRequest(hermeService, clazz, method, args);
         if (!TextUtils.isEmpty(responce.getData())) {
             ResponceBean responceBean = GSON.fromJson(responce.getData(), ResponceBean.class);
             if (responceBean.getData() != null) {
                 Object getUserReslut = responceBean.getData();
+
                 String data = GSON.toJson(getUserReslut);
-//
-                Class stringgetUser = method.getReturnType();
+
+                Class stringgetUser = method.getReturnType();   //getReturnType():该对象表示的方法的返回类型
+
                 Object o = GSON.fromJson(data, stringgetUser);
                 return o;
 
--- architecture/src/main/java/com/blend/architecture/eventbus/hermesevent/HermesEventBusMainActivity.java
+++ architecture/src/main/java/com/blend/architecture/eventbus/hermesevent/HermesEventBusMainActivity.java
@@ -13,6 +13,7 @@ import com.blend.architecture.eventbus.model.Friend;
 
 /**
  * Hermes是之前饿了么开源的一套专门用于跨进程通信的框架，基于aidl进行封装，但是在使用的时候完全不用考虑aidl。
+ * github:https://github.com/736870598/SunHermes
  *
  * 使用流程：
  *  1.假设A进程为主进程，B进程为其他进程。
@@ -20,6 +21,18 @@ import com.blend.architecture.eventbus.model.Friend;
  *  3.A进程中要有一个单例类实现该接口。
  *  4.在B进程中该接口类上面要加上注解 @ClassId("实现类的全路径")
  *  5.A进程中接口的实现类必须是单例的，而且获取单例方法名必须是：getInstance()
+ *
+ * Hermes的思路：
+ * 1.A进程保存单例类信息到键值对集合中，分别保存类和方法。
+ * 2.B进程连接到跨进程服务。
+ * 3.B进程传入单例类接口，在B进程中通过动态代理拿到单例类的实例；并把这个单例类接口的全类名（通过注解的方式写在接口定义上），方法和参数，进行GSON序列化，通过AIDL传递给A进程
+ * 4.A进程通过GSON反序列化后，根据全类名，通过反射得到单例类的实例，并通过反射调用相关的方法后得到结果，通过AIDL传递给B进程完成跨进程调用
+ *
+ * 为什么要使用单例？
+ * 因为使用单例，反射后得到的还是单例，就能调用之前使用这个单例存储的值。
+ *
+ * 动态代理的原理？
+ *
  */
 public class HermesEventBusMainActivity extends AppCompatActivity {
 
