8177cdfe2d6f4c6413a1610db5a47c0384a61c05
BlendAndroid <1059152860@qq.com>
2020-07-28 11:19:48 +0800

    GlideMainActivity

---
+++ architecture/src/main/java/com/blend/architecture/glide/GlideMainActivity.java
@@ -0,0 +1,18 @@
+package com.blend.architecture.glide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.blend.architecture.R;
+
+/**
+ *
+ */
+public class GlideMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glide_main);
+    }
+}
