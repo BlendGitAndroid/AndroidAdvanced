842741a4679c7853c709c28b4c554d897fb1ede4
BlendAndroid <1059152860@qq.com>
2020-09-03 16:24:56 +0800

    RxJava概念总结

--- architecture/src/main/java/com/blend/architecture/rxjava/RxJavaMainActivity.java
+++ architecture/src/main/java/com/blend/architecture/rxjava/RxJavaMainActivity.java
@@ -32,7 +32,27 @@ import io.reactivex.schedulers.Schedulers;
 /**
  * 设计模式：装饰着模式，观察者模式
  * 叫你去搭建RxJava框架，你会怎么设计？
- * 三要素：观察者，被观察者，订阅(被观察者订阅观察者)
+ * <p>
+ *
+ * <p>
+ * 简介：一个可观测的序列来组成异步的、基于事件的程序的库。
+ * 好处：RxJava其实就是提供一套异步编程的API，这套API是基于观察者模式的，而且是链式调用的，所以使用RxJava编写的代码的逻辑会非常简洁。
+ * 使用场景：数据库的读写、大图片的载入、文件压缩/解压等各种需要放在后台工作的耗时操作
+ * 三要素：被观察者(五种)，观察者，订阅。被观察者订阅观察者
+ * 操作符：创建操作符、转换操作符、组合操作符、功能操作符、过滤操作符、条件操作符
+ * <p>
+ *
+ * <p>
+ * 线程调度：简化了异步操作
+ * subscribeOn：指定被观察者的线程，通过接收一个Scheduler参数，来指定对数据的处理运行在特定的线程调度器Scheduler上，若多次设定，则只有一次起作用。
+ * observeOn：指定观察者的线程，接收一个Scheduler参数，来指定下游操作运行在特定的线程调度器Scheduler上，若多次设定，每次均起作用。
+ * <p>
+ *
+ * <p>-
+ * 背压：当上下游在不同的线程中，通过Observable发射，处理，响应数据流时，如果上游发射数据的速度快于下游接收处理数据的速度，这样对于那些没来得及处理的数
+ * 据就会造成积压，这些数据既不会丢失，也不会被垃圾回收机制回收，而是存放在一个异步缓存池中，如果缓存池中的数据一直得不到处理，越积越多，最后就会造成内存
+ * 溢出，这便是响应式编程中的背压（backpressure）问题。
+ * 解决思路：响应式拉取，响应式拉取是观察者主动去被观察者那里拉取事件，而被观察者则是被动等待通知再发射事件。
  */
 public class RxJavaMainActivity extends RxAppCompatActivity {
 
@@ -43,6 +63,8 @@ public class RxJavaMainActivity extends RxAppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_java_main);
 
+        create();
+
         just();
 
         flowable();
@@ -50,6 +72,36 @@ public class RxJavaMainActivity extends RxAppCompatActivity {
         interval();
 
         customize();
+    }
+
+    private void create() {
+        io.reactivex.Observable.create(new io.reactivex.ObservableOnSubscribe<String>() {
+
+            @Override
+            public void subscribe(io.reactivex.ObservableEmitter<String> emitter) throws Exception {
+
+            }
+        }).subscribe(new io.reactivex.Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
     }
 
     private void just() {
