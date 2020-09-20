76d556869614b26ea8910fbd0af2ea5722c8b8bf
BlendAndroid <1059152860@qq.com>
2020-09-20 15:38:08 +0800

    泛型通配符

--- routercompiler/src/main/java/com/blend/routercompiler/Genericity.java
+++ routercompiler/src/main/java/com/blend/routercompiler/Genericity.java
@@ -171,6 +171,9 @@ public class Genericity {
      * 最后看一下什么是PECS（Producer Extends Consumer Super）原则，已经很好理解了：
      * 频繁往外读取内容的，适合用上界Extends。
      * 经常往里插入的，适合用下界Super。
+     *
+     *
+     * 所以，在平常的使用过程中，super一般用于参数传递，而extend一般用作返回值。
      */
 
     /**
