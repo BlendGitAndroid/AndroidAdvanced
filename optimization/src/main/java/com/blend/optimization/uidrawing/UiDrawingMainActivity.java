9b3e685dfc866579d185ecbea461de27f59e16c1
BlendAndroid <1059152860@qq.com>
2020-09-22 09:45:44 +0800

    绘制优化

---
+++ optimization/src/main/java/com/blend/optimization/uidrawing/UiDrawingMainActivity.java
@@ -0,0 +1,15 @@
+package com.blend.optimization.uidrawing;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.blend.optimization.R;
+
+public class UiDrawingMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui_drawing_main);
+    }
+}
