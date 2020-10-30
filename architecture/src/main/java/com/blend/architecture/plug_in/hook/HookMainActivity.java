ae31ed2fd65226e3babda5f1ff749d103ba5dbc1
BlendAndroid <1059152860@qq.com>
2020-10-30 18:20:02 +0800

    Android Click Hook事件

--- architecture/src/main/java/com/blend/architecture/plug_in/hook/HookMainActivity.java
+++ architecture/src/main/java/com/blend/architecture/plug_in/hook/HookMainActivity.java
@@ -2,30 +2,98 @@ package com.blend.architecture.plug_in.hook;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.blend.architecture.R;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 /**
  * Hook技术介绍：
  * 钩子函数实际上是一个处理消息的程序段，通过系统调用，把它挂入系统。在系统没有到调用该函数之前，钩子程序就先捕获该消息，这样钩子函数先得到控制权。
  * 这时钩子函数即可以加工处理（改变）该函数的执行行为,还可以强制结束消息的传递。
- * HOOK技术实现途径
+ * Hook 的这个本领，使它能够将自身的代码「融入」被勾住（Hook）的程序的进程中，成为目标进程的一个部分。API Hook 技术是一种用于改变 API 执行结
+ * 果的技术，能够将系统的 API 函数执行重定向。在 Android 系统中使用了沙箱机制，普通用户程序的进程空间都是独立的，程序的运行互不干扰。这就使我们
+ * 希望通过一个程序改变其他程序的某些行为的想法不能直接实现，但是 Hook 的出现给我们开拓了解决此类问题的道路。当然，根据 Hook 对象与 Hook 后处理
+ * 的事件方式不同，Hook 还分为不同的种类，比如消息 Hook、API Hook 等。
+ * <p>
+ * HOOK技术实现途径：
  * 第一 :找到hook点
  * 第二 :将hook方法放到系统之外执行
  * <p>
- * 本例子是Hook AMS服务实现大型登陆架构，与AOP进行比较。
+ * 本例子是Hook：
+ * 1.API Hook，Android的各种监听。
+ * 2.AMS服务实现大型登陆架构，与AOP进行比较。
+ * 网址：https://www.jianshu.com/p/4f6d20076922
  */
 
 public class HookMainActivity extends AppCompatActivity {
+
+    private static final String TAG = "HookMainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hook_main);
+        hookTest();
+        hookOnClickListener(findViewById(R.id.hookTest));
+    }
+
+    private void hookTest() {
+        findViewById(R.id.hookTest).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "点击事件");
+            }
+        });
+    }
+
+
+    /**
+     * 这个代码要放在设置Click事件之后
+     */
+    private void hookOnClickListener(View view) {
+        try {
+            // 得到 View 的 ListenerInfo 对象
+            Method getListenerInfo = View.class.getDeclaredMethod("getListenerInfo");
+            getListenerInfo.setAccessible(true);
+            Object listenerInfo = getListenerInfo.invoke(view);
+
+            // 得到 原始的 OnClickListener 对象
+            Class<?> listenerInfoClz = Class.forName("android.view.View$ListenerInfo");
+            Field mOnClickListener = listenerInfoClz.getDeclaredField("mOnClickListener");
+            mOnClickListener.setAccessible(true);
+            View.OnClickListener originOnClickListener = (View.OnClickListener) mOnClickListener.get(listenerInfo);
+
+            // 用自定义的 OnClickListener 替换原始的 OnClickListener
+            View.OnClickListener hookedOnClickListener = new HookedOnClickListener(originOnClickListener);
+            mOnClickListener.set(listenerInfo, hookedOnClickListener);  //在这里替换赋值
+        } catch (Exception e) {
+            Log.e(TAG, "hookOnClickListener: ");
+        }
+    }
+
+    class HookedOnClickListener implements View.OnClickListener {
+        private View.OnClickListener origin;
+
+        HookedOnClickListener(View.OnClickListener origin) {
+            this.origin = origin;
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(HookMainActivity.this, "hook click", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "onClick: Before click, do what you want to to.");
+            if (origin != null) {
+                origin.onClick(v);
+            }
+            Log.e(TAG, "After click, do what you want to to.");
+        }
     }
 
     public void jump2(View view) {
