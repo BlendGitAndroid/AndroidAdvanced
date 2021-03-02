63523736b7ef0cf40e54419f6367a668cc49b49c
BlendAndroid <1059152860@qq.com>
2021-03-02 13:46:59 +0800

    RecyclerView

--- ui/src/main/java/com/blend/ui/recyclerview/RecyclerViewActivity.java
+++ ui/src/main/java/com/blend/ui/recyclerview/RecyclerViewActivity.java
@@ -4,9 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.blend.ui.R;
@@ -17,6 +16,7 @@ import java.util.List;
 public class RecyclerViewActivity extends AppCompatActivity {
 
     private RecyclerView rv;
+    private Button clickRv;
     private int[] iv = new int[]{
             R.drawable.duo,
     };
@@ -27,6 +27,7 @@ public class RecyclerViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recycler_view2);
 
         rv = findViewById(R.id.rv);
+        clickRv = findViewById(R.id.clickRv);
         rv.setLayoutManager(new GridLayoutManager(this, 1));
 
         final List<Integer> list = new ArrayList<>();
@@ -35,6 +36,15 @@ public class RecyclerViewActivity extends AppCompatActivity {
         }
         final RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, list);
         rv.setAdapter(adapter);
+
+        clickRv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                list.set(2, R.drawable.avatar);
+                adapter.notifyItemInserted(2);
+            }
+        });
+
 
         adapter.setOnItemClickListener(position -> Toast.makeText(RecyclerViewActivity.this, "第几个：" + position, Toast.LENGTH_SHORT).show());
     }
