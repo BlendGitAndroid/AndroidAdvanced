11992fa88717e7170b248148c8b8dcb7a06ce25a
BlendAndroid <1059152860@qq.com>
2021-03-08 18:50:43 +0800

    CoordinatorLayout

--- ui/src/main/java/com/blend/ui/material_design/MaterialDesignMainActivity.java
+++ ui/src/main/java/com/blend/ui/material_design/MaterialDesignMainActivity.java
@@ -42,8 +42,9 @@ public class MaterialDesignMainActivity extends AppCompatActivity implements Nav
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //         .setAction("Action", null).show();
+                startActivity(new Intent(MaterialDesignMainActivity.this, MaterialTestActivity.class));
             }
         });
 
---
+++ ui/src/main/java/com/blend/ui/material_design/MaterialTestActivity.java
@@ -0,0 +1,15 @@
+package com.blend.ui.material_design;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.blend.ui.R;
+
+public class MaterialTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_test);
+    }
+}
