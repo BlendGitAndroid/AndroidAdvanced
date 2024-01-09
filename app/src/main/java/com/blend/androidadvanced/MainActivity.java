4ebfd94d21745664e1371857464f66e86c7bee78
BlendAndroid <1059152860@qq.com>
2024-01-09 18:54:10 +0800

    增加权限

--- app/src/main/java/com/blend/androidadvanced/MainActivity.java
+++ app/src/main/java/com/blend/androidadvanced/MainActivity.java
@@ -1,8 +1,11 @@
 package com.blend.androidadvanced;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 
 import com.blend.architecture.ArchitectureMainActivity;
 import com.blend.ui.UiMainActivity;
@@ -34,6 +37,30 @@ public class MainActivity extends Activity {
 
         //数据结构与算法
         // AlgorithmClass.algorithm();
+
+        verifyStoragePermissions();
         finish();
+    }
+
+    public void verifyStoragePermissions() {
+        int REQUEST_EXTERNAL_STORAGE = 1;
+
+        String[] PERMISSIONS = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO};
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS, REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
